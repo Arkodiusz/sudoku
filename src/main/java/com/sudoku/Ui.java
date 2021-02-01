dc00cb0ea89c2d6984eafe6a464aceccf6311798
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-02-01 18:00:42 +0100

    conflict displaying moved to Ui Class

--- src/main/java/com/sudoku/Ui.java
+++ src/main/java/com/sudoku/Ui.java
@@ -5,20 +5,85 @@ import java.util.Scanner;
 public class Ui {
     Scanner sc = new Scanner(System.in);
 
+    public void printBoard(int[][] board) {
+        String out_horizontal =   "# # # # # # # # # # # # # # # # # # # # # # # # # # # #\n";
+        String in_horizontal  = "\n# - - + - - + - - # - - + - - + - - # - - + - - + - - #\n";
+
+        StringBuilder grid = new StringBuilder();
+
+        for(int i=0; i<9; i++) {
+            if(i==0 || i==3 || i==6) grid.append(out_horizontal);
+            for(int j=0; j<9; j++) {
+                if (j==0) grid.append("#  ");
+
+                int val = board[i][j];
+                if (val!=0) grid.append(val);
+                else grid.append(" ");
+
+                if(j==2 || j==5) grid.append("  #  ");
+                else if(j==8) grid.append("  #");
+                else grid.append("  |  ");
+            }
+            if(i!=2 && i!=5 && i!=8) grid.append(in_horizontal);
+            else if (i!=8) grid.append("\n");
+        }
+        grid.append("\n").append(out_horizontal);
+        System.out.print(grid);
+    }
+
+    public void printConflicts(Conflicts conflicts) {
+        if(!conflicts.getColumns().isEmpty()) {
+            System.out.print("Conflicts in columns: ");
+            conflicts.getColumns().forEach(i -> System.out.print((i + 1) + " "));
+            System.out.print("\n");
+        }
+
+        if(!conflicts.getRows().isEmpty()) {
+            System.out.print("Conflicts in rows: ");
+            conflicts.getRows().forEach(i -> System.out.print((i + 1) + " "));
+            System.out.print("\n");
+        }
+
+        if(!conflicts.getBoxes().isEmpty()) {
+            System.out.print("Conflicts in boxes: ");
+            conflicts.getBoxes().forEach(i -> System.out.print((i + 1) + " "));
+            System.out.print("\n");
+        }
+    }
+
     public Cell getCell() {
         int x;
         int y;
         int val;
 
-        //validation
+        System.out.println("\nEdit cell");
+
         System.out.print("column = ");
-        x = sc.nextInt()-1;
+        x = validateInput() - 1;
         System.out.print("row = ");
-        y = sc.nextInt()-1;
+        y = validateInput() - 1;
         System.out.print("value = ");
-        val = sc.nextInt();
+        val = validateInput();
 
         return new Cell (x, y, val);
     }
 
+    private int validateInput() {
+        int i = 0;
+        int number;
+        do {
+            i++;
+            while (!sc.hasNextInt()) {
+                i++;
+                System.out.println("That's not a number!");
+                sc.next();
+            }
+            number = sc.nextInt();
+            if (i!=0 && (number < 1 || number > 9)) {
+                System.out.println("Out of bounds!");
+            }
+        } while (number < 1 || number > 9);
+        System.out.println("Thank you! Got " + number);
+        return number;
+    }
 }
